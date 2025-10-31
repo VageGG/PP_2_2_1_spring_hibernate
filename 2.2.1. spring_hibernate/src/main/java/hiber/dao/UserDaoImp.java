81a3f1b4fda2d4880d6aa527349f1938ac6ac951
Vage Gevorgyan <Vage13@mail.ru>
2025-10-31 14:58:04 +0300

    Fix join fetch for second method in userdaoimpl

--- 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/dao/UserDaoImp.java
@@ -29,7 +29,7 @@ public class UserDaoImp implements UserDao {
 
    @Override
    public User getUserByCarModelAndSeries(String model, int series) {
-      String hql = "SELECT u FROM User u WHERE u.car.model = :model AND u.car.series = :series";
+      String hql = "SELECT u FROM User u JOIN FETCH u.car c WHERE c.model = :model AND c.series = :series";
       TypedQuery<User> query=sessionFactory.getCurrentSession().createQuery(hql, User.class);
       query.setParameter("model", model);
       query.setParameter("series", series);
